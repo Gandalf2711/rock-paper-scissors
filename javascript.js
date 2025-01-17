f1881fb244f3024a0c5a39c2de7224cc9b9391e4
Gandalf2711 <190136093+Gandalf2711@users.noreply.github.com>
2025-01-17 20:59:27 +0100

    Add playGame func that runs 5 rounds of the game

--- javascript.js
+++ javascript.js
@@ -1,9 +1,3 @@
-let computerScore = 0;
-let humanScore = 0;
-
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-
 // Return a random integer between 0 and 2
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -35,5 +29,43 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// Run the game
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+
+    //Give the result of a round
+    function playRound(computerChoice, humanChoice) {
+        if(computerChoice == humanChoice) {
+            console.log("It's a tie!");
+        } else if ((computerChoice == "Rock" && humanChoice == "Scissors") || (computerChoice == "Paper" && humanChoice == "Rock") || (computerChoice == "Scissors" && humanChoice == "Paper")) {
+            console.log("Computer wins! " + computerChoice + " beats " + humanChoice);
+            computerScore = computerScore + 1;
+            console.log("Computer's score: " + computerScore);
+        } else {
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+            humanScore = humanScore + 1;
+            console.log("Your score: " + humanScore);
+        }
+    } 
+
+    //Run 5 rounds
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+
+        console.log("Computer's choice: " + computerSelection);
+        console.log("Your choice: " + humanSelection);
+        console.log(playRound(computerSelection,humanSelection));
+    }
+
+    //Give the winner of the game
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won!");
+    } else {
+        console.log("Computer won! You'll do better next time!");
+    }
+
+}
+
+console.log(playGame());
